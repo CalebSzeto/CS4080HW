6f4b3ad150f4069a375d67b98b59d243210a905c
calebszeto <caleb.a.szeto@gmail.com>
2026-03-03 15:34:22 -0800

    add ch 10 && ch 11 answers

---
+++ HW5/ch10/ch10Q2/RpnPrinter.java
@@ -0,0 +1,65 @@
+package com.craftinginterpreters.lox;
+
+class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitTernaryExpr(Expr.Ternary expr) {
+        return expr.condition.accept(this) + " " + expr.thenBranch.accept(this) + " " + expr.elseBranch.accept(this) + " ?:";
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitAssignExpr(Expr.Assign expr) {
+        return expr.value.accept(this) + " " + expr.name.lexeme + " =";
+    }
+
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        return expr.name.lexeme;
+    }
+
+    @Override
+    public String visitCallExpr(Expr.Call expr) {
+        StringBuilder result = new StringBuilder();
+        
+        for (Expr argument : expr.arguments) {
+            result.append(argument.accept(this)).append(" ");
+        }
+        result.append(expr.callee.accept(this)).append(" call");
+        
+        return result.toString();
+    }
+
+    @Override
+    public String visitFunctionExpr(Expr.Function expr) {
+        return "fun"; 
+    }
+
+    @Override
+    public String visitLogicalExpr(Expr.Logical expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+}
---
+++ HW5/ch11/ch11Q4/Environment.java
@@ -0,0 +1,85 @@
+package com.craftinginterpreters.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Environment {
+  final Environment enclosing;
+  private final Map<String, Object> values = new HashMap<>();
+  private final List<Object> slots = new ArrayList<>();
+  
+  private static final Object UNINITIALIZED = new Object();
+
+  Environment() {
+    enclosing = null;
+  }
+
+  Environment(Environment enclosing) {
+    this.enclosing = enclosing;
+  }
+
+  Object get(Token name) {
+    if (values.containsKey(name.lexeme)) {
+      Object value = values.get(name.lexeme);
+      if (value == UNINITIALIZED) {
+        throw new RuntimeError(name, "Variable '" + name.lexeme + "' is used before being initialized.");
+      }
+      return value;
+    }
+
+    if (enclosing != null) return enclosing.get(name);
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void assign(Token name, Object value) {
+    if (values.containsKey(name.lexeme)) {
+      values.put(name.lexeme, value);
+      return;
+    }
+
+    if (enclosing != null) {
+      enclosing.assign(name, value);
+      return;
+    }
+
+    throw new RuntimeError(name, "Undefined variable '" + name.lexeme + "'.");
+  }
+
+  void define(String name, Object value) {
+    values.put(name, value);
+    slots.add(value);
+  }
+  
+  void defineUninitialized(String name) {
+    values.put(name, UNINITIALIZED);
+    slots.add(UNINITIALIZED);
+  }
+
+  Object getAtIndex(int distance, int index) {
+    return ancestor(distance).slots.get(index);
+  }
+
+  void assignAtIndex(int distance, int index, Object value) {
+    ancestor(distance).slots.set(index, value);
+  }
+  
+  Object getAt(int distance, String name) {
+    return ancestor(distance).values.get(name);
+  }
+
+  void assignAt(int distance, Token name, Object value) {
+    ancestor(distance).values.put(name.lexeme, value);
+  }
+
+  Environment ancestor(int distance) {
+    Environment environment = this;
+    for (int i = 0; i < distance; i++) {
+      environment = environment.enclosing;
+    }
+
+    return environment;
+  }
+}
