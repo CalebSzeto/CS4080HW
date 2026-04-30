ae6c3df7b15faeb8755f6c1d238840629cb0d138
calebszeto <caleb.a.szeto@gmail.com>
2026-04-30 01:14:35 -0700

    added ch24 and ch25 code implementation

---
+++ HW12/ch24/ch24Q3/object.h
@@ -0,0 +1,90 @@
+#ifndef clox_object_h
+#define clox_object_h
+#include "common.h"
+#include "chunk.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+#define IS_CLOSURE(value)      isObjType(value, OBJ_CLOSURE)
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
+#define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+#define IS_UPVALUE(value)      isObjType(value, OBJ_UPVALUE)
+
+#define AS_CLOSURE(value)      ((ObjClosure*)AS_OBJ(value))
+#define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
+#define AS_NATIVE_OBJ(value)   ((ObjNative*)AS_OBJ(value))
+#define AS_NATIVE(value) \
+    (AS_NATIVE_OBJ(value)->function)
+#define AS_NATIVE_ARITY(value) (AS_NATIVE_OBJ(value)->arity)
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+#define AS_UPVALUE(value)      ((ObjUpvalue*)AS_OBJ(value))
+
+typedef enum {
+  OBJ_CLOSURE,
+  OBJ_FUNCTION,
+  OBJ_NATIVE,
+  OBJ_STRING,
+  OBJ_UPVALUE,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+  struct Obj* next;
+};
+
+typedef struct {
+  Obj obj;
+  int arity;
+  int upvalueCount;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
+typedef struct ObjUpvalue ObjUpvalue;
+
+typedef struct {
+  Obj obj;
+  ObjFunction* function;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
+} ObjClosure;
+
+typedef bool (*NativeFn)(int argCount, Value* args, Value* result);
+
+typedef struct {
+  Obj obj;
+  NativeFn function;
+  int arity;
+} ObjNative;
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char* chars;
+  uint32_t hash;
+  bool ownsChars;
+};
+
+struct ObjUpvalue {
+  Obj obj;
+  Value* location;
+  Value closed;
+  struct ObjUpvalue* next;
+};
+
+ObjClosure* newClosure(ObjFunction* function);
+ObjFunction* newFunction(void);
+ObjNative* newNative(NativeFn function, int arity);
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+ObjString* constantString(const char* chars, int length);
+ObjUpvalue* newUpvalue(Value* slot);
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
---
+++ HW12/ch25/ch25Q2/chunk.h
@@ -0,0 +1,62 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+  int line;
+  int runLength;
+} LineRun;
+
+typedef enum {
+  OP_CONSTANT,
+  OP_CONSTANT_LONG,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_POP,
+  OP_GET_LOCAL,
+  OP_SET_LOCAL,
+  OP_GET_GLOBAL,
+  OP_DEFINE_GLOBAL,
+  OP_SET_GLOBAL,
+  OP_GET_UPVALUE,
+  OP_SET_UPVALUE,
+  OP_PRINT,
+  OP_JUMP,
+  OP_JUMP_IF_FALSE,
+  OP_LOOP,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_NOT,
+  OP_NEGATE,
+  OP_CALL,
+  OP_CLOSURE,
+  OP_CLOSE_UPVALUE,
+  OP_CLOSE_UPVALUE_AT,
+  OP_RETURN,
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  int lineCount;
+  int lineCapacity;
+  LineRun* lines;
+  ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void writeConstant(Chunk* chunk, Value value, int line);
+int getLine(Chunk* chunk, int instruction);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
