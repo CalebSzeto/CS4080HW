6fb41c336e47a8a084a5f2bdbf14f6c6382783e0
calebszeto <caleb.a.szeto@gmail.com>
2026-05-07 12:36:30 -0700

    add ch28&29 code implementation

---
+++ HW14/ch28/ch28Q1/object.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <string.h>
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+#include "vm.h"
+
+static Obj* allocateObject(size_t size, ObjType type) {
+  Obj* object = (Obj*)reallocate(NULL, 0, size);
+  object->type = type;
+  object->isMarked = false;
+  object->next = vm.objects;
+  vm.objects = object;
+#if DEBUG_LOG_GC
+  printf("%p allocate %zu for %d\n", (void*)object, size, type);
+#endif
+  return object;
+}
+
+ObjClosure* newClosure(ObjFunction* function) {
+  ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
+  for (int i = 0; i < function->upvalueCount; i++) {
+    upvalues[i] = NULL;
+  }
+
+  ObjClosure* closure = (ObjClosure*)allocateObject(sizeof(ObjClosure), OBJ_CLOSURE);
+  closure->function = function;
+  closure->upvalues = upvalues;
+  closure->upvalueCount = function->upvalueCount;
+  return closure;
+}
+
+ObjBoundMethod* newBoundMethod(Value receiver, ObjClosure* method) {
+  ObjBoundMethod* bound = (ObjBoundMethod*)allocateObject(sizeof(ObjBoundMethod), OBJ_BOUND_METHOD);
+  bound->receiver = receiver;
+  bound->method = method;
+  return bound;
+}
+
+ObjClass* newClass(ObjString* name) {
+  ObjClass* klass = (ObjClass*)allocateObject(sizeof(ObjClass), OBJ_CLASS);
+  klass->name = name;
+  initTable(&klass->methods);
+  klass->initializer = NIL_VAL;
+  return klass;
+}
+
+ObjFunction* newFunction(void) {
+  ObjFunction* function = (ObjFunction*)allocateObject(sizeof(ObjFunction), OBJ_FUNCTION);
+  function->arity = 0;
+  function->upvalueCount = 0;
+  function->name = NULL;
+  initChunk(&function->chunk);
+  return function;
+}
+
+ObjInstance* newInstance(ObjClass* klass) {
+  ObjInstance* instance = (ObjInstance*)allocateObject(sizeof(ObjInstance), OBJ_INSTANCE);
+  instance->klass = klass;
+  initTable(&instance->fields);
+  return instance;
+}
+
+ObjNative* newNative(NativeFn function, int arity) {
+  ObjNative* native = (ObjNative*)allocateObject(sizeof(ObjNative), OBJ_NATIVE);
+  native->function = function;
+  native->arity = arity;
+  return native;
+}
+
+static uint32_t hashString(const char* key, int length) {
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < length; i++) {
+    hash ^= (uint8_t)key[i];
+    hash *= 16777619;
+  }
+  return hash;
+}
+
+static ObjString* allocateString(char* chars, int length, uint32_t hash, bool ownsChars) {
+  ObjString* string = (ObjString*)allocateObject(sizeof(ObjString), OBJ_STRING);
+  string->length = length;
+  string->chars = chars;
+  string->hash = hash;
+  string->ownsChars = ownsChars;
+  push(OBJ_VAL(string));
+  tableSet(&vm.strings, OBJ_VAL(string), NIL_VAL);
+  pop();
+  return string;
+}
+
+ObjString* takeString(char* chars, int length) {
+  uint32_t hash = hashString(chars, length);
+  ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+  if (interned != NULL) {
+    FREE_ARRAY(char, chars, length + 1);
+    return interned;
+  }
+
+  return allocateString(chars, length, hash, true);
+}
+
+ObjString* copyString(const char* chars, int length) {
+  uint32_t hash = hashString(chars, length);
+  ObjString* interned = tableFindString(&vm.strings, chars, length, hash);
+  if (interned != NULL) return interned;
+
+  char* heapChars = ALLOCATE(char, length + 1);
+  memcpy(heapChars, chars, length);
+  heapChars[length] = '\0';
+  return allocateString(heapChars, length, hash, true);
+}
+
+ObjString* constantString(const char* chars, int length) {
+  return copyString(chars, length);
+}
+
+ObjUpvalue* newUpvalue(Value* slot) {
+  ObjUpvalue* upvalue = (ObjUpvalue*)allocateObject(sizeof(ObjUpvalue), OBJ_UPVALUE);
+  upvalue->closed = NIL_VAL;
+  upvalue->location = slot;
+  upvalue->next = NULL;
+  return upvalue;
+}
+
+static void printFunction(ObjFunction* function) {
+  if (function->name == NULL) {
+    printf("<script>");
+    return;
+  }
+  printf("<fn %s>", function->name->chars);
+}
+
+void printObject(Value value) {
+  switch (OBJ_TYPE(value)) {
+    case OBJ_BOUND_METHOD:
+      printFunction(AS_BOUND_METHOD(value)->method->function);
+      break;
+    case OBJ_CLASS:
+      printf("%s", AS_CLASS(value)->name->chars);
+      break;
+    case OBJ_CLOSURE:
+      printFunction(AS_CLOSURE(value)->function);
+      break;
+    case OBJ_FUNCTION:
+      printFunction(AS_FUNCTION(value));
+      break;
+    case OBJ_INSTANCE:
+      printf("%s instance", AS_INSTANCE(value)->klass->name->chars);
+      break;
+    case OBJ_NATIVE:
+      printf("<native fn>");
+      break;
+    case OBJ_STRING: {
+      ObjString* string = AS_STRING(value);
+      printf("%.*s", string->length, string->chars);
+      break;
+    }
+    case OBJ_UPVALUE:
+      printf("upvalue");
+      break;
+  }
+}
---
+++ HW14/ch29/ch29Q3/object.h
@@ -0,0 +1,132 @@
+#ifndef clox_object_h
+#define clox_object_h
+#include "common.h"
+#include "chunk.h"
+#include "table.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)        (AS_OBJ(value)->type)
+
+#define IS_BOUND_METHOD(value) isObjType(value, OBJ_BOUND_METHOD)
+#define IS_CLASS(value)        isObjType(value, OBJ_CLASS)
+#define IS_CLOSURE(value)      isObjType(value, OBJ_CLOSURE)
+#define IS_FUNCTION(value)     isObjType(value, OBJ_FUNCTION)
+#define IS_INSTANCE(value)     isObjType(value, OBJ_INSTANCE)
+#define IS_NATIVE(value)       isObjType(value, OBJ_NATIVE)
+#define IS_STRING(value)       isObjType(value, OBJ_STRING)
+#define IS_UPVALUE(value)      isObjType(value, OBJ_UPVALUE)
+
+#define AS_BOUND_METHOD(value) ((ObjBoundMethod*)AS_OBJ(value))
+#define AS_CLASS(value)        ((ObjClass*)AS_OBJ(value))
+#define AS_CLOSURE(value)      ((ObjClosure*)AS_OBJ(value))
+#define AS_FUNCTION(value)     ((ObjFunction*)AS_OBJ(value))
+#define AS_INSTANCE(value)     ((ObjInstance*)AS_OBJ(value))
+#define AS_NATIVE_OBJ(value)   ((ObjNative*)AS_OBJ(value))
+#define AS_NATIVE(value) \
+    (AS_NATIVE_OBJ(value)->function)
+#define AS_NATIVE_ARITY(value) (AS_NATIVE_OBJ(value)->arity)
+#define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
+#define AS_UPVALUE(value)      ((ObjUpvalue*)AS_OBJ(value))
+
+typedef enum {
+  OBJ_BOUND_METHOD,
+  OBJ_CLASS,
+  OBJ_CLOSURE,
+  OBJ_FUNCTION,
+  OBJ_INSTANCE,
+  OBJ_NATIVE,
+  OBJ_STRING,
+  OBJ_UPVALUE,
+} ObjType;
+
+struct Obj {
+  ObjType type;
+  bool isMarked;
+  struct Obj* next;
+};
+
+typedef struct {
+  Obj obj;
+  int arity;
+  int upvalueCount;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
+
+typedef struct ObjUpvalue ObjUpvalue;
+typedef struct ObjClosure ObjClosure;
+typedef struct ObjClass ObjClass;
+typedef struct ObjInstance ObjInstance;
+typedef struct ObjBoundMethod ObjBoundMethod;
+
+struct ObjBoundMethod {
+  Obj obj;
+  Value receiver;
+  ObjClosure* method;
+};
+
+struct ObjClass {
+  Obj obj;
+  ObjString* name;
+  struct ObjClass* superclass;
+  Table methods;
+  Table innerMethods;
+  Value initializer;
+};
+
+struct ObjInstance {
+  Obj obj;
+  ObjClass* klass;
+  Table fields;
+};
+
+struct ObjClosure {
+  Obj obj;
+  ObjFunction* function;
+  ObjClass* ownerClass;
+  ObjString* methodName;
+  ObjUpvalue** upvalues;
+  int upvalueCount;
+};
+
+typedef bool (*NativeFn)(int argCount, Value* args, Value* result);
+
+typedef struct {
+  Obj obj;
+  NativeFn function;
+  int arity;
+} ObjNative;
+
+struct ObjString {
+  Obj obj;
+  int length;
+  char* chars;
+  uint32_t hash;
+  bool ownsChars;
+};
+
+struct ObjUpvalue {
+  Obj obj;
+  Value* location;
+  Value closed;
+  struct ObjUpvalue* next;
+};
+
+ObjClosure* newClosure(ObjFunction* function);
+ObjFunction* newFunction(void);
+ObjBoundMethod* newBoundMethod(Value receiver, ObjClosure* method);
+ObjClass* newClass(ObjString* name);
+ObjInstance* newInstance(ObjClass* klass);
+ObjNative* newNative(NativeFn function, int arity);
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+ObjString* constantString(const char* chars, int length);
+ObjUpvalue* newUpvalue(Value* slot);
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#endif
